02275fcefd1442d80b88c3a33a57a5d46a305da3
Antoine Aube <aaube3@gmail.com>
2023-07-18 14:00:11 -0600

    feat: popular model/popular model year

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -22,7 +22,33 @@ class VehicleFactory
     # @created_vehicles
   end
 
-  def popular_make_model
-    
+  def popular_make_model(vehicle_list)
+    make_and_model_array = []
+    vehicle_list.each do |vehicle|
+      make_and_model = "#{vehicle.make} #{vehicle.model}"
+      make_and_model_array << make_and_model
+    end 
+    #group_by = creates hash with unique key for each unique element in the array
+    #with all occurences of that element in an array as the key value
+    most_popular_as_hash = make_and_model_array.group_by do |make_model|
+       make_model
+    end 
+    #.values returns an array of all the value arrays from the above hash
+    #max_by(&:size) return the array with the largest size
+    #(&:size)is equivalent to {|array| array.size}
+    #.first returns the first element of that largest array (which is the
+    #same as all the elements in the array, but produces a single string
+    #for the purpose of this method)
+    most_popular = most_popular_as_hash.values.max_by(&:size).first
+  end
+
+  def popular_vehicles_model_year(year)
+    model_year_count = 0
+    @created_vehicles.each do |vehicle|
+      if vehicle.year == year
+        model_year_count += 1
+      end
+    end
+    model_year_count
   end
 end
