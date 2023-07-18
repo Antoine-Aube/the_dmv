1ffa3522959538d5a4f9236678eae557d09790d9
Antoine Aube <aaube3@gmail.com>
2023-07-18 13:59:08 -0600

    test: popular make and model/popular vehicles model year

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -21,4 +21,27 @@ RSpec.describe VehicleFactory do
       expect(created_registrations).to all be_a Vehicle
     end
   end
+  
+  describe "#popular_make_model" do 
+    it "find the most popular make and model" do 
+      factory_1 = VehicleFactory.new
+      registrations = DmvDataService.new.wa_ev_registrations
+      factory_1.create_vehicles(registrations)
+      created_registrations = factory_1.created_vehicles
+      # require 'pry';binding.pry
+      expect(factory_1.popular_make_model(created_registrations)).to eq("NISSAN Leaf")
+    end
+  end
+  
+  describe "#popular_vehicles_model_year" do 
+    it "can count the number of cars based on model year from registerd vehicles" do 
+      factory_1 = VehicleFactory.new
+      registrations = DmvDataService.new.wa_ev_registrations
+      factory_1.create_vehicles(registrations)
+      created_registrations = factory_1.created_vehicles
+      # require 'pry';binding.pry
+      expect(factory_1.popular_vehicles_model_year("2015")).to eq(57)
+      expect(factory_1.popular_vehicles_model_year("2021")).to eq(113)
+    end 
+  end
 end
